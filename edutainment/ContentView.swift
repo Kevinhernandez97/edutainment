59ac6cfae97fa43bbe034f867cde512ea7c5342b
Kevin Hernandez <kmhs-18@hotmail.com>
2023-03-27 12:11:55 -0300

    Initial Commit

---
+++ edutainment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  edutainment
+//
+//  Created by Kevin Hernandez on 27/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Kevin Hernandez on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
